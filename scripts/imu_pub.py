1dafb60b8674e03c6beb3d930ccf001bf4909457
doyle34 <parkdi0409@gmail.com>
2021-08-06 17:59:56 +0900

    created new pub code

--- scripts/imu_pub.py
+++ scripts/imu_pub.py
@@ -40,8 +40,6 @@ def talker():
     #print(ser.readline())
 
     imu_pub = rospy.Publisher("imu_data", Imu, queue_size=5)
-    odom_pub = rospy.Publisher("odom", Odometry, queue_size=5)
-    odom_broadcaster = tf.TransformBroadcaster()
     stab_broadcaster = tf.TransformBroadcaster()
     rate = rospy.Rate(10) # 10hz
     x = 0
@@ -66,7 +64,7 @@ def talker():
         imu_data.header.frame_id = "base_link"
         imu_data.orientation.z = float(str_list[0])
         imu_data.orientation.y = float(str_list[1])
-        imu_data.orientation.x = float(str_list[2])
+        imu_data.orientation.x = -float(str_list[2])
         imu_data.orientation.w = float(str_list[3])
         imu_data.linear_acceleration.x = 0
         imu_data.linear_acceleration.y = 0
@@ -78,61 +76,56 @@ def talker():
         imu_data.angular_velocity_covariance[0] = -1
         imu_pub.publish(imu_data)
 
-        current_time = rospy.Time.now()
-        dt = (current_time - last_time).to_sec()
-        vx = float(str_list[7])
-        vy = float(str_list[8])
-        dx = vx * dt
-        dy = vy * dt
-        x += dx
-        y += dy
-
-        odom_quat = (float(str_list[2]),float(str_list[1]),float(str_list[0]),float(str_list[3]))
-        euler = tf.transformations.euler_from_quaternion(odom_quat)
-        stab_quat = tf.transformations.quaternion_from_euler(euler[0],-euler[1],0)
+        # odom_quat = (float(str_list[2]),float(str_list[1]),float(str_list[0]),float(str_list[3]))
+        # euler = tf.transformations.euler_from_quaternion(odom_quat)
+        # tf.transformat
+        # stab_quat = tf.transformations.quaternion_from_euler(euler[0],-euler[1],0)
+        # (imu_data.orientation.z, imu_data.orientation.y, imu_data.orientation.x, imu_data.orientation.w)
         stab_broadcaster.sendTransform(
             (0, 0, 0),
-            stab_quat,
+            (imu_data.orientation.x, imu_data.orientation.y, imu_data.orientation.z, imu_data.orientation.w),
             rospy.Time.now(),
-            "base_link",
-            "base_footprint"
+            "map",
+            "base_link"
         )
 
-        odom_data.pose.pose.orientation.z = float(str_list[0])
-        odom_data.pose.pose.orientation.y = float(str_list[1])
-        odom_data.pose.pose.orientation.x = float(str_list[2])
-        odom_data.pose.pose.orientation.w = float(str_list[3])
-        odom_data.pose.pose.position.x = x
-        odom_data.pose.pose.position.y = y
-        odom_data.pose.pose.position.z = 0
-        odom_data.twist.twist.linear.x = vx
-        odom_data.twist.twist.linear.y = vy
-        odom_data.twist.twist.linear.z = 0
-        odom_data.twist.twist.angular.x = 0
-        odom_data.twist.twist.angular.y = 0
-        odom_data.twist.twist.angular.z = float(str_list[6])
 
-        # first, we'll publish the transform over tf
-        odom_broadcaster.sendTransform(
-            (x, y, 0.0),
-            odom_quat,
-            rospy.Time.now(),
-            "base_footprint",
-            "odom"
-        )
 
-        # next, we'll publish the odometry message over ROS
-        odom_data.header.stamp = rospy.Time.now()
-        odom_data.header.frame_id = "odom"
+        # odom_data.pose.pose.orientation.z = float(str_list[0])
+        # odom_data.pose.pose.orientation.y = float(str_list[1])
+        # odom_data.pose.pose.orientation.x = float(str_list[2])
+        # odom_data.pose.pose.orientation.w = float(str_list[3])
+        # odom_data.pose.pose.position.x = x
+        # odom_data.pose.pose.position.y = y
+        # odom_data.pose.pose.position.z = 0
+        # odom_data.twist.twist.linear.x = vx
+        # odom_data.twist.twist.linear.y = vy
+        # odom_data.twist.twist.linear.z = 0
+        # odom_data.twist.twist.angular.x = 0
+        # odom_data.twist.twist.angular.y = 0
+        # odom_data.twist.twist.angular.z = float(str_list[6])
 
-        # set the velocity
-        odom_data.child_frame_id = "base_footprint"
+        # # # first, we'll publish the transform over tf
+        # odom_broadcaster.sendTransform(
+        #     (0.0, 0.0, 0.0),
+        #     odom_quat,
+        #     rospy.Time.now(),
+        #     "odom", 
+        #     "base_footprint"
+        # )
 
-        # publish the message
-        odom_pub.publish(odom_data)
-        last_time = current_time
+        # # next, we'll publish the odometry message over ROS
+        # odom_data.header.stamp = rospy.Time.now()
+        # odom_data.header.frame_id = "odom"
 
-        prev_str = str_list
+        # # set the velocity
+        # odom_data.child_frame_id = "base_footprint"
+
+        # # publish the message
+        # odom_pub.publish(odom_data)
+        # last_time = current_time
+
+        # prev_str = str_list
         # end_time = time.time()
         rate.sleep()
 
